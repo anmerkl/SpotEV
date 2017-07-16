51829469c55d6994aa2d5469ad1104a39864ad19
Gandhi <sgandhi@hq.corp.viasat.com>
2017-07-16 02:30:13 -0700

    Move to python library to overcome wifi difficulties

---
+++ HardwareSide/ultraSonicSensor/spotSensors.py
@@ -0,0 +1,50 @@
+import mraa
+import time
+
+from subprocess import call
+
+TRIG = mraa.Gpio(11)
+ECHO = mraa.Gpio(10)
+TRIG2 = mraa.Gpio(6)
+ECHO2 = mraa.Gpio(5)
+
+TRIG.dir(mraa.DIR_OUT)
+ECHO.dir(mraa.DIR_IN)
+TRIG2.dir(mraa.DIR_OUT)
+ECHO2.dir(mraa.DIR_IN)
+
+
+def poll(tP, eP):
+    tP.write(0)
+    time.sleep(0.0002)
+
+    tP.write(1)
+    time.sleep(0.00001)
+    tP.write(0)
+
+    while eP.read() == 0:
+        pass
+    pulse_start = time.time()
+
+    while eP.read() == 1:
+        pass
+    pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+
+    distance = pulse_duration * 17150
+
+    distance = round(distance, 2)
+    print "Distance:", distance, " cm"
+
+
+def main():
+    while (1):
+        poll(TRIG, ECHO)
+        time.sleep(10)
+
+
+if __name__ == "__main__":
+    main()
+
+# call(["curl", "-L", "google.com"])
