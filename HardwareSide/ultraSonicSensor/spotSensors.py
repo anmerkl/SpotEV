e71efd747f043bc34c3d76a071373e0c978c954b
Gandhi <sgandhi@hq.corp.viasat.com>
2017-07-16 04:44:22 -0700

    Implement the curl commands as well as full sensor integration

--- HardwareSide/ultraSonicSensor/spotSensors.py
+++ HardwareSide/ultraSonicSensor/spotSensors.py
@@ -15,10 +15,6 @@ ECHO2.dir(mraa.DIR_IN)
 
 NUM_SAMPLES = 200
 SUCCESS_PERCENT = 0.60
-countOne = 0
-countTwo = 0
-carOneTime = 0
-carTwoTime = 0
 
 
 def poll(tP, eP):
@@ -51,18 +47,60 @@ def poll(tP, eP):
 
 
 def main():
-    while (1):
-        for (var i in range(0, NUM_SAMPLES)):
-            poll(TRIG, ECHO)
+    currTime = time.time()
+    pastTime = time.time()
+    countOne = 0
+    countTwo = 0
+    carOneTime = 0
+    carTwoTime = 0
 
-        poll(TRIG2, ECHO2)
+    loc_1 = 'Building10-A'
+    loc_2 = 'Building10-B'
+    occupiedOne = 0
+    occupiedTwo = 0
+
+    while (1):
+        for i in range(0, NUM_SAMPLES):
+            countOne += poll(TRIG, ECHO)
+            time.sleep(0.002)
+        if NUM_SAMPLES * SUCCESS_PERCENT < countOne:
+            print "Object one found!"
+            carOneTime += NUM_SAMPLES * 0.002
+            carOneTime += 1.5
+            occupiedOne = 1
+        else:
+            print "Object is not there"
+            carOneTime = 0
+            occupiedOne = 0
+
+        for i in range(0, NUM_SAMPLES):
+            countTwo += poll(TRIG2, ECHO2)
+            time.sleep(0.002)
+        if NUM_SAMPLES * SUCCESS_PERCENT < countTwo:
+            print "Object two found!"
+            carTwoTime += NUM_SAMPLES * 0.002
+            carTwoTime += 1.5
+            occupiedTwo = 1
+        else:
+            print "Object is not there"
+            carTwoTime = 0
+            occupiedTwo = 0
+
+        countOne = 0
+        countTwo = 0
+
+        currTime = time.time()
+        if (currTime - pastTime >= 30):
+            s = 'curl -v -H \'Content-Type: application/json\' -H \'Accept: application/json\' -X POST -d \'{{ \"spot\" : {{ \"location\": \"{0}\", \"occupied\": {1}, \"prev_occupy_duration\": {2} }} }} \' http://spotev.hack.viasat.io:8080/spot'.format(
+                str(loc_1), str(occupiedOne), str(carOneTime))
+            st = 'curl -v -H \'Content-Type: application/json\' -H \'Accept: application/json\' -X POST -d \'{{ \"spot\" : {{ \"location\": \"{0}\", \"occupied\": {1}, \"prev_occupy_duration\": {2} }} }} \' http://spotev.hack.viasat.io:8080/spot'.format(
+                str(loc_2), str(occupiedTwo), str(carTwoTime))
+            os.system(s)
+            os.system(st)
+            pastTime = currTime
 
         time.sleep(1)
 
 
 if __name__ == "__main__":
-    # os.system(
-    #     'curl -v -H \'Content-Type: application/json\' -H \'Accept: application/json\' -X POST -d \'{ \"spot\": { \"location\": \"building 10\", \"occupied\": true, \"prev_occupy_duration\": 134 } }\' http://spotev.hack.viasat.io:8080/spot')
     main()
-
-# call(["curl", "-L", "google.com"])
