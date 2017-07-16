e52b6a4f4b234ab8ac5cae5f1081931318e89ec2
Alfonso Merkl <alfonso.merkl@viasat.com>
2017-07-16 05:05:37 -0700

    slight improvements to maps display

--- spotev/app/controllers/home_controller.rb
+++ spotev/app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
       @hash = Gmaps4rails.build_markers(@spots) do |spot, marker|
         marker.lat spot.location_lat
         marker.lng spot.location_long
-        marker.infowindow 'LMAO'
+        marker.infowindow spot.free_count_report
       end
     end
 end
--- spotev/app/models/spot.rb
+++ spotev/app/models/spot.rb
@@ -1,11 +1,28 @@
 class Spot < ApplicationRecord
 
   def location_lat
-    self.location == 'building 10' ? 33.127910519621246 : 33.127928489402585
+    (location.include? '10') ? 33.127910519621246 : 33.127928489402585
   end
 
   def location_long
-    self.location == 'building 10' ? -117.2653412793909 : -117.26436495530947
+    (location.include? '10') ? -117.2653412793909 : -117.26436495530947
+  end
+
+  def free_count_report
+    (location.include? '10') ? "There are #{bldg10_free_count} free spots" :
+                               "There are #{bldg11_free_count} free spots"
+  end
+
+  def bldg10_free_count
+    count = 0
+    Spot.all.each { |s| count += 1 if (!s.occupied && s.location.include?('10')) }
+    count
+  end
+
+  def bldg11_free_count
+    count = 0
+    Spot.all.each { |s| count += 1 if (!s.occupied && s.location.include?('11')) }
+    count
   end
 
 end
